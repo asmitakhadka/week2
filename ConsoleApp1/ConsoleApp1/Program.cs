d3f01adee9cbf3f7fceec12bc02796112b2ec185
Asmita Khadka <10015309@student.toiohomai.ac.nz>
2018-07-30 16:50:13 +1200

    class acitivity

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello, please enter your name");
+            string input = Console.ReadLine();
+            Console.Write("hello");
+            Console.Write(input);
+
+            Console.WriteLine("please input a age");
+            string input1 = Console.ReadLine();
+            Console.Write("age");
+            Console.Write(input1);
+
+            Console.WriteLine("please enter a gender");
+            string input3 = Console.ReadLine();
+            Console.Write("gender");
+            Console.Write(input3);
+
+            Console.WriteLine("please enter a address");
+            string input4 = Console.ReadLine();
+            Console.Write("address");
+            Console.Write(input4);
+
+
+        }
+    }
+}
